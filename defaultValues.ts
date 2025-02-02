e76b2c69c89051db07c477bba4380c09d1916ee9
Flávia Correia <fllaviacorreia@outlook.com>
2025-02-02 10:10:10 -0300

    create default values

---
+++ defaultValues.ts
@@ -0,0 +1,11 @@
+
+export interface DrawPath {
+  path: string;
+  color: string;
+  width: number;
+  colorFrom: "pencil" | "eraser";
+}
+
+export const colorsPencil = ["red", "blue", "black", "yellow", "white", "green", "brown"];
+export const colorsBackground = ["black", "white", "gray", "#2C5F2D"];
+export const sizeIcons = 30;
